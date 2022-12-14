9cc9f944e2232eaaa6a9573d1e357c500d31e9d4
N1colas7 <nicolasnd9897@gmail.com>
2022-12-14 17:10:00 +0100

    Insert Image and create array object

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Creiamo l'array di oggetti
+let oggetti = [
+    {
+        img : "01.webp",
+        title : "Spiderman Miles Morales",
+        text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
+    },
+    {
+        img : "02.webp",
+        title : "Rachet & Clank",
+        text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
+    },
+    {
+        img : "03.webp",
+        title : "Fortnite",
+        text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
+    },
+    {
+        img : "04.webp",
+        title : "A misterious cat",
+        text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
+    },
+    {
+        img : "05.webp",
+        title : "Marvel Avengers",
+        text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
+    },
+]
