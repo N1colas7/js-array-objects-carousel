bb5a845a4d30cb06d8be239770a6f904e4f65cd3
N1colas7 <nicolasnd9897@gmail.com>
2022-12-14 19:58:49 +0100

    button function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
 // Creiamo l'array di oggetti
 let oggetti = [
     {
-        id : 1,
         img : "01.webp",
         title : "Spiderman Miles Morales",
         text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
     },
     {
-        id : 2,
         img : "02.webp",
         title : "Rachet & Clank",
         text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
     },
     {
-        id : 3,
         img : "03.webp",
         title : "Fortnite",
         text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
     },
     {
-        id : 4,
         img : "04.webp",
         title : "A misterious cat",
         text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
     },
     {
-        id : 5,
         img : "05.webp",
         title : "Marvel Avengers",
         text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, itaque harum non suscipit natus aliquid."
     },
 ]
+
+let objectId = 0;
+
+function showSlide(){
+    let principalImage = document.getElementById("bigImage");
+    principalImage.innerHTML=`
+    <img class="image_size" src="./img/${oggetti[objectId].img}" alt="">
+    <p>${oggetti[objectId].title}</p>
+    <p>${oggetti[objectId].text}</p>`
+}
+showSlide();
 
 // Aggiungiamo le immagini, il titolo, e il testo tramite js
 const itemsContent = document.getElementById('containerLittleImage')
@@ -40,7 +46,29 @@ for(let i = 0; i < oggetti.length; i++){
         <img src="./img/${oggetti[i].img}" width="250px" height="116px"/>
         `
     }
+
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
     
-let imageCounter = 1;
-const first = document.getElementById('imageFirst');
-first.src = "/img/0" +  imageCounter + ".webp"
+next.addEventListener('click', function(){
+    if(objectId == 4){
+        objectId = 0
+        showSlide()
+    }else{
+        objectId++
+        showSlide()
+    }
+    console.log("successivo " + objectId + " " + oggetti[objectId].img)
+});
+prev.addEventListener('click', function(){
+    if(objectId == 0){
+        objectId = 4
+        showSlide()
+    }else{
+        objectId = objectId -1
+        showSlide()
+    }
+    console.log("successivo " + objectId + " " + oggetti[objectId].img)
+})
+
+
